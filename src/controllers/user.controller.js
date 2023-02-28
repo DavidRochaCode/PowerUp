9d1f22479017345bfb13355572bd388129a525ad
David Rocha <manoeudavi3@gmail.com>
2023-02-28 10:01:38 -0300

    implementar função de criptografar senha dos usuários

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import{create_usuario} from '../repositories/usuario.repository'
+import bcrypt from 'bcrypt'
 
 export const create = async(req,res)=>{
     try {
+        const hashPassword = await bcrypt.hash(req.body.senha, 10)
+        req.body.senha = hashPassword
         const usuario = await create_usuario(req.body)
         res.status(200).send(usuario)
     } catch (error) {
--- src/repositories/usuario.repository.js
+++ src/repositories/usuario.repository.js
@@ -2,7 +2,22 @@ import { prisma } from "../services/prisma";
 
 export const create_usuario = async(data) =>{
     const user = await prisma.usuario.create({
-        data
+        data,
+        select:{
+            id:true,
+            nome:true,
+            senha:false,
+            email:true,
+            peso:true,
+            altura:true,
+            genero:true,
+            fator_atividade:true,
+            imc:true,
+            dieta:true,
+
+
+
+        }
     })
     return user
 }
