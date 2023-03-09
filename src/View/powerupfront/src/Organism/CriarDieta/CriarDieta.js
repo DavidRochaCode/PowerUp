d71b741fe3560c0ab27dd40ee644e32c4d4cc6b3
David Rocha <manoeudavi3@gmail.com>
2023-03-09 07:13:46 -0300

    adicioando algumas informações

--- src/View/powerupfront/src/Organism/CriarDieta/CriarDieta.js
+++ src/View/powerupfront/src/Organism/CriarDieta/CriarDieta.js
@@ -3,6 +3,8 @@ import { Buttons } from "../../Atomic/Buttons/Buttons";
 import { DietaForm } from "../DietaForm/DietaForm";
 
 export function CriarDieta(){
+
+    
     return(
         <div className="flex flex-col items-center justify-center">
             <div className=" w-max h-max p-[50px] shadow-2xl rounded-[12px] my-[30px] ">
