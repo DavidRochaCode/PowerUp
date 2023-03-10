871973f954d0b758bd3cfda6a5a65e821797803f
David Rocha <manoeudavi3@gmail.com>
2023-03-10 09:43:28 -0300

    adicionado função de cadastrar alimentos

--- src/View/powerupfront/src/Organism/CriarDieta/CriarDieta.js
+++ src/View/powerupfront/src/Organism/CriarDieta/CriarDieta.js
@@ -110,17 +110,76 @@ export function CriarDieta(){
         quantidade: quantidadeJanta,
     }
 
-    function addRefeicao(){
+    function addCafe(event){
+        //event.preventDefault()
         axios
             .post("http://localhost:3001/cafe", cafe)
             .then(response => {
               if (response.status === 200) {
-                console.log("usuário cadastrado");
-                alert("Você foi cadastrado")
+                console.log("café cadastrado");
+                alert("Café cadastrado")
               }
             })
             .catch((err) => {
               console.log(err);
+            });
+    }
+
+    function addLancheM(event){
+      //event.preventDefault()
+        axios
+            .post("http://localhost:3001/lanche-da-manha", lancheManha)
+            .then(response => {
+              if (response.status === 200) {
+                console.log("Lanche cadastrado");
+                alert("Lanche cadastrado")
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+    }
+
+    function addAlmoco(){
+        axios
+            .post("http://localhost:3001/almoco", almoco)
+            .then(response => {
+              if (response.status === 200) {
+                console.log("Almoco cadastrado");
+                alert("Almoco cadastrado")
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+    }
+
+    function addLancheT(){
+        axios
+            .post("http://localhost:3001/lanche-da-tarde", lancheTarde)
+            .then(response => {
+              if (response.status === 200) {
+                console.log("Lanche cadastrado");
+                alert("Lanche cadastrado")
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+    }
+
+    function addJantar(){
+        axios
+            .post("http://localhost:3001/jantar", jantar)
+            .then(response => {
+              if (response.status === 200) {
+                console.log("Jantar cadastrado");
+                alert("jantar cadastrado")
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              alert(err)
             });
     }
 
@@ -130,19 +189,19 @@ export function CriarDieta(){
                 <p>Dieta</p>
                 <div className="grid grid-cols-10 gap-4">
                     <div className="col-span-2">
-                        <DietaForm name="Café" nomeFunc={handleNomeCafe} quantidadeFunc={handleQtCafe} proteinaFunc={handleQtCafeProt} carboidratoFunc={handleQtCafeCarb} gorduraFunc={handleQtCafeGord} />
+                        <DietaForm name="Café" nomeFunc={handleNomeCafe} quantidadeFunc={handleQtCafe} proteinaFunc={handleQtCafeProt} carboidratoFunc={handleQtCafeCarb} gorduraFunc={handleQtCafeGord} butFunc = {addCafe} />
                     </div>
                     <div className="col-span-2">
-                        <DietaForm name="Lanche Manhã" nomeFunc={handleNomeLancheM} quantidadeFunc={handleQtLancheM} proteinaFunc={handleQtLancheMProt} carboidratoFunc={handleQtLancheMCarb} gorduraFunc={handleQLancheMGord} />
+                        <DietaForm name="Lanche Manhã" nomeFunc={handleNomeLancheM} quantidadeFunc={handleQtLancheM} proteinaFunc={handleQtLancheMProt} carboidratoFunc={handleQtLancheMCarb} gorduraFunc={handleQLancheMGord} butFunc = {addLancheM} />
                     </div>
                     <div className="col-span-2">
-                        <DietaForm name="Almoço" nomeFunc={handleNomeAlmoco} quantidadeFunc={handleQtAlmoco} proteinaFunc={handleQtAlmocoProt} carboidratoFunc={handleQtAlmocoCarb} gorduraFunc={handleQtAlmocoAGord} />
+                        <DietaForm name="Almoço" nomeFunc={handleNomeAlmoco} quantidadeFunc={handleQtAlmoco} proteinaFunc={handleQtAlmocoProt} carboidratoFunc={handleQtAlmocoCarb} gorduraFunc={handleQtAlmocoAGord} butFunc = {addAlmoco} />
                     </div>
                     <div className="col-span-2">
-                        <DietaForm name="Lanche Tarde" nomeFunc={handleNomeLancheT} quantidadeFunc={handleQtLancheT} proteinaFunc={handleQtLancheTProt} carboidratoFunc={handleQtLancheTCarb} gorduraFunc={handleQtLancheTGord} />
+                        <DietaForm name="Lanche Tarde" nomeFunc={handleNomeLancheT} quantidadeFunc={handleQtLancheT} proteinaFunc={handleQtLancheTProt} carboidratoFunc={handleQtLancheTCarb} gorduraFunc={handleQtLancheTGord} butFunc = {addLancheT} />
                     </div>
                     <div className="col-span-2">
-                        <DietaForm name="Janta" nomeFunc={handleNomeJanta} quantidadeFunc={handleQtJanta} proteinaFunc={handleQtJantaProt} carboidratoFunc={handleQtJantaCarb} gorduraFunc={handleQtJantaAGord} />
+                        <DietaForm name="Janta" nomeFunc={handleNomeJanta} quantidadeFunc={handleQtJanta} proteinaFunc={handleQtJantaProt} carboidratoFunc={handleQtJantaCarb} gorduraFunc={handleQtJantaAGord} butFunc = {addJantar} />
                     </div>
                 </div>
                 
